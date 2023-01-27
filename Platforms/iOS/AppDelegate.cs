778ab4f29279799af864443062f0c317771e0f8d
JimHinke <jimmiehinke@gmail.com>
2023-01-27 15:25:09 +0100

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace OPC_inlamningsuppgift3;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
